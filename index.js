20a98ae10bbfe6c76406ad1bfd29851d63922051
Glitch (glitch-blank-node) <none>
2022-03-17 01:38:45 +0000

    💿👣 Checkpoint ./index.js:43062906/421 ./config.json:43062906/181 ./deploy-commands.js:43062906/1211

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const { Client, Intents } = require('discord.js');
 const dotenv = require('dotenv');
 dotenv.config();
 
-
+// Token
+const token = process.env.DISCORD_TOKEN
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
@@ -14,6 +15,22 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 client.once('ready', () => {
 	console.log('Ready!');
 });
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
+
+
 
 // Login to Discord with your client's token
 client.login(token);
