7ad6a65f01f0510adacf5b8ea41fb0b686d89e5a
Glitch (glitch-blank-node) <none>
2022-03-17 01:26:45 +0000

    🗻👚 Checkpoint ./index.js:43062906/947 ./package.json:43062906/110 ./config.json:43062906/200

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
+
+// .env
+const dotenv = require('dotenv');
+dotenv.config();
+
+
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
